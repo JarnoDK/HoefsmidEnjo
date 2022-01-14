b5c99265720e29fb8d9d94cb2f4d726cb5598961
Jarno Dekeyser <jarno.dekeyser.y9019@student.hogent.be>
2022-01-14 13:21:03 +0100

    add more start data

--- Services/Data/DataInitializer.cs
+++ Services/Data/DataInitializer.cs
@@ -30,15 +30,40 @@ namespace Services.Data
 
         private void FillData()
         {
-            _dbContext.Users.Add(new User("Jarno", "Dekeyser", "Wachtwoord", "0468", "Jarnodekeyser@hotmail.be", "04 92 88 72 59", HoefsmidEnjo.Shared.Users.UserRole.Klant));
-            _dbContext.Users.Add(new User("Enjo", "Dekeyser", "Wachtwoord", "0468", "Enjodekeyser@hotmail.be", "04 92 88 72 59", HoefsmidEnjo.Shared.Users.UserRole.Klant));
+
+            User user = new("Jarno", "Dekeyser", "Wachtwoord", "0468", "Jarnodekeyser@hotmail.be", "04 92 88 72 59", HoefsmidEnjo.Shared.Users.UserRole.Klant);
+            User user2 = new("Enjo", "Dekeyser", "Wachtwoord", "0468", "Enjodekeyser@hotmail.be", "04 92 88 72 59", HoefsmidEnjo.Shared.Users.UserRole.Klant);
+
+            _dbContext.Users.Add(user);
+            _dbContext.Users.Add(user2);
 
             _dbContext.Events.Add(new Event("Warm beslag Miro", "Manege Partege", DateTime.Now, 1));
-            _dbContext.Events.Add(new Event("Jumping", "Manege sunkawakan", DateTime.Now.AddDays(1).AddHours(-6).AddMinutes(-7), 1));
+            _dbContext.Events.Add(new Event("Jumping", "Manege sunkawakan", DateTime.Now.AddDays(1).AddHours(-6).AddMinutes(-7), 2));
 
             _dbContext.Items.Add(new InvoiceItem("Linker voorpoot bekappen",35.00));
             _dbContext.Items.Add(new InvoiceItem("Controle", 10.00));
+            _dbContext.SaveChanges();
 
+
+            _dbContext.Invoices.Add(
+                    new Invoice(
+                            DateTime.Now,
+                            new() {
+                                new InvoiceLine(5,1),
+                                new InvoiceLine(3,2)
+                            },
+                            1
+                            ));
+
+            _dbContext.Invoices.Add(
+                new Invoice(
+                    DateTime.Now,
+                    new()
+                    {
+                        new InvoiceLine(3, 1)
+                    },
+                    2
+                ));
 
             _dbContext.SaveChanges();
 
